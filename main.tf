43558ba28e20d61b673b289e27b4d7f3a3a1533b
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-21 19:52:49 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ami" {
-  ami = "data.aws_ami.ami.id"
+  ami = data.aws_ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-05c45d4106139409c"]
   tags = {
